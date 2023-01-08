3e4d5b5b0884699129648adff88cb0444d2ff02d
Mathias-Boulay <mathiasboulay@free.fr>
2023-01-08 17:38:55 +0100

    Reset GL Error after the hardware tests (for #394)

--- src/glx/hardext.c
+++ src/glx/hardext.c
@@ -51,6 +51,7 @@ static int testGLSL(const char* version, int uniformLoc) {
     LOAD_GLES2(glCompileShader);
     LOAD_GLES2(glGetShaderiv);
     LOAD_GLES2(glDeleteShader);
+    LOAD_GLES(glGetError);
 
     GLuint shad = gles_glCreateShader(GL_VERTEX_SHADER);
     const char* shadTest[4] = {
@@ -75,6 +76,7 @@ static int testGLSL(const char* version, int uniformLoc) {
     }
     */
     gles_glDeleteShader(shad);
+    gl4es_glGetError();	// reset GL Error
 
     return compiled;
 }
@@ -85,6 +87,7 @@ static int testTextureCubeLod() {
     LOAD_GLES2(glCompileShader);
     LOAD_GLES2(glGetShaderiv);
     LOAD_GLES2(glDeleteShader);
+    LOAD_GLES(glGetError);
 
     GLuint shad = gles_glCreateShader(GL_FRAGMENT_SHADER);
     const char* shadTest[3] = {
@@ -102,6 +105,7 @@ static int testTextureCubeLod() {
     GLint compiled;
     gles_glGetShaderiv(shad, GL_COMPILE_STATUS, &compiled);
     gles_glDeleteShader(shad);
+    gl4es_glGetError(); // reset GL Error
 
     return compiled;
 }
