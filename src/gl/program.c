c7f4f5348d1f1e810701e476db9eb559ae823c03
Mathias-Boulay <mathiasboulay@free.fr>
2023-02-20 00:32:13 +0100

    uniform: Fetch uniform location for each array elements

--- src/gl/program.c
+++ src/gl/program.c
@@ -570,6 +570,8 @@ static void fill_program(program_t *glprogram)
                     if(j) {
                         gluniform->name = malloc(strlen(name)+1+5);
                         sprintf(gluniform->name, "%s[%d]", name, j);
+                        // Renew id with index
+                        id = gles_glGetUniformLocation(glprogram->id, gluniform->name);
                     } else
                         gluniform->name = strdup(name);
                     gluniform->id = id;
@@ -593,7 +595,6 @@ static void fill_program(program_t *glprogram)
                     }
                     DBG(printf(" uniform #%d : \"%s\"%s type=%s size=%d\n", id, gluniform->name, gluniform->builtin?" (builtin) ":"", PrintEnum(gluniform->type), gluniform->size);)
                     if(gluniform->size==1) ++glprogram->num_uniform;
-                    id++;
                 }
                 uniform_cache += uniformsize(type)*size;
             }
