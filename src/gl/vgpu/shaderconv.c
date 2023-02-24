a75422251465ab72b8010484ddbbbd88616c37a1
SerpentSpirale <mathiasboulay@free.fr>
2023-02-25 00:14:49 +0100

    Fix: Don't test base gl4es shaders when forced vgpu pipeline is enabled

--- src/gl/vgpu/shaderconv.c
+++ src/gl/vgpu/shaderconv.c
@@ -22,11 +22,12 @@ char * ConvertShaderConditionally(struct shader_s * shader_source){
 
     // First, vanilla gl4es, no forward port
     shader_source->converted = ConvertShader(shader_source->source, shader_source->type == GL_VERTEX_SHADER ? 1 : 0,&shader_source->need, 0);
-    shaderCompileStatus = testGenericShader(shader_source);
+    if(!globals4es.vgpu_force_conv)  // Skip the test, consider it uncompiled
+        shaderCompileStatus = testGenericShader(shader_source);
 
     // Then, attempt back porting if desired of constrained to do so
     if(!shaderCompileStatus && globals4es.vgpu_backport) {
-        shader_source->converted = ConvertShader(shader_source->source, shader_source->type == GL_VERTEX_SHADER ? 1 : 0,&shader_source->need, 0);
+        //shader_source->converted = ConvertShader(shader_source->source, shader_source->type == GL_VERTEX_SHADER ? 1 : 0,&shader_source->need, 0);
         shader_source->converted = ConvertShaderVgpu(shader_source);
         shaderCompileStatus = testGenericShader(shader_source);
     }
