fef134fef40b195a1e241443c42f4db1eb0ee3a9
Mathias-Boulay <mathiasboulay@free.fr>
2023-02-25 22:29:26 +0100

    Cleanup:

--- src/gl/vgpu/shaderconv.c
+++ src/gl/vgpu/shaderconv.c
@@ -18,7 +18,7 @@ int NO_OPERATOR_VALUE = 9999;
  * @return The shader as a string
  */
 char * ConvertShaderConditionally(struct shader_s * shader_source){
-    int shaderCompileStatus;
+    int shaderCompileStatus = 0;
 
     // First, vanilla gl4es, no forward port
     shader_source->converted = ConvertShader(shader_source->source, shader_source->type == GL_VERTEX_SHADER ? 1 : 0,&shader_source->need, 0);
@@ -27,7 +27,6 @@ char * ConvertShaderConditionally(struct shader_s * shader_source){
 
     // Then, attempt back porting if desired of constrained to do so
     if(!shaderCompileStatus && globals4es.vgpu_backport) {
-        //shader_source->converted = ConvertShader(shader_source->source, shader_source->type == GL_VERTEX_SHADER ? 1 : 0,&shader_source->need, 0);
         shader_source->converted = ConvertShaderVgpu(shader_source);
         shaderCompileStatus = testGenericShader(shader_source);
     }
