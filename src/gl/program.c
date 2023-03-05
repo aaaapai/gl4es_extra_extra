f4363d69d106de60b7f54db74ef49dcca9b4db7d
Mathias-Boulay <mathiasboulay@free.fr>
2023-03-05 16:01:27 +0100

    Feat: Log linker errors

--- src/gl/program.c
+++ src/gl/program.c
@@ -800,6 +800,15 @@ void APIENTRY_GL4ES gl4es_glLinkProgram(GLuint program) {
             fill_program(glprogram);
             noerrorShimNoPurge();
         } else {
+            GLsizei log_length;
+            gles_glGetProgramiv(glprogram->id, GL_INFO_LOG_LENGTH, &log_length);
+            DBG(printf("Linker error length: %i\n", log_length));
+            if(log_length != 0) {
+               LOAD_GLES2(glGetProgramInfoLog);
+               GLchar log_chars[log_length];
+               gles_glGetProgramInfoLog(glprogram->id, log_length, &log_length, log_chars);
+               printf("%s\n", log_chars);
+            }
             // should DBG the linker error?
             DBG(printf(" Link failled!\n");)
             glprogram->linked = 0;
