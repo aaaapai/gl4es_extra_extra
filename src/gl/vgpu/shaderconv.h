3dbc017e753f0602d89675bbfbda0fc9bd81fa9f
Mathias-Boulay <mathiasboulay@free.fr>
2023-03-07 09:41:55 +0100

    Feat: Handle switch-case statements

--- src/gl/vgpu/shaderconv.h
+++ src/gl/vgpu/shaderconv.h
@@ -41,6 +41,7 @@ char * ReplaceFragmentOut(char * source, int *sourceLength);
 char * BackportConstArrays(char *source, int * sourceLength);
 char * ExtractString(char * source, int startString, int endString);
 char * RemoveUniformProperty(char * source);
+char* ProcessSwitchCases(char* source, int* length);
 
 
 char* GetOperandFromOperator(char* source, int operatorIndex, int rightOperand, int * limit);
