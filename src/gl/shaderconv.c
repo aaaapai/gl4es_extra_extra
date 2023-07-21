c860afa54789dc50357e45b54f1dc922266a092c
Mathias Boulay <mathiasboulay@free.fr>
2023-07-21 14:43:32 +0200

    Cut[shaderconv]:Remove forward/backward port headers

--- src/gl/shaderconv.c
+++ src/gl/shaderconv.c
@@ -1263,13 +1263,14 @@ char* ConvertShader(const char* pEntry, int isVertex, shaderconv_need_t *need, i
     Tmp = gl4es_inplace_replace(Tmp, &tmpsize, "mat3x3", "mat3");
   }
 
+  /*
   if (versionHeader > 1) {
     const char* GLESBackport = "#define texture2D texture\n#define attribute in\n#define varying out\n";
     Tmp = gl4es_inplace_insert(gl4es_getline(Tmp, 1), GLESBackport, Tmp, &tmpsize);
   }else {
       const char* GLESForwardPort = "#define texture texture2D\n #define textureProj texture2DProj\n #define mod(a,b) (int(a) - int(b) * int(a/b))\n";
       Tmp = gl4es_inplace_insert(gl4es_getline(Tmp, 1), GLESForwardPort, Tmp, &tmpsize);
-  }
+  }*/
   
   // finish
   if((globals4es.dbgshaderconv&maskafter)==maskafter) {
