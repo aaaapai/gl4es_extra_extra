6f0b0cffd53ff969655c52ea5af7edf1a00695af
ArtDev <45949002+artdeell@users.noreply.github.com>
2023-09-06 09:07:18 +0300

    Fix[fix]: use -> instead of .

--- src/glsl_optimizer/src/code/GlslConvert.cpp
+++ src/glsl_optimizer/src/code/GlslConvert.cpp
@@ -155,7 +155,7 @@ char * GlslConvert::Optimize(
     bool isESShader,
 	OptimizationStruct vOptimizationStruct)
 {
-	this.failed = false;
+	this->failed = false;
     char * optimized_shader = NULL;
 
 	struct gl_shader* shader = rzalloc(NULL, struct gl_shader);
